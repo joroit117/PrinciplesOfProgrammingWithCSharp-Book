4979b3c45e599d06b5aa22b9e4f42033325f8212
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-03-25 13:19:14 +0200

    Add files via upload

---
+++ Theme 2 - PrimitiveTypesAndVariables/task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EvenOrNot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Uneven");
+            }
+        }
+    }
+}
---
+++ Theme 2 - PrimitiveTypesAndVariables/task2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _5and7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            if (a % 5 == 0 && a % 7 == 0)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+        }
+    }
+}
