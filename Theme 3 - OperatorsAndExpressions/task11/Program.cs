a8a757a83002f212bb0a6bf01ed088cf16b3c411
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-03-25 18:26:07 +0200

    Add files via upload

---
+++ Theme 3 - OperatorsAndExpressions/task11/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace task11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+            int i = 1;
+            int mask = i << p;
+
+            Console.WriteLine((n & mask) != 0 ? 1 : 0);
+
+        }
+    }
+}
