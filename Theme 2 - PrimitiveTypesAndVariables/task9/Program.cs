2612ac167b9ea50f5f10913268c387a8d346cfbc
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-03-25 13:26:47 +0200

    Add files via upload

---
+++ Theme 2 - PrimitiveTypesAndVariables/task9/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("x = ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("y = ");
+            int y = int.Parse(Console.ReadLine());
+
+            Console.Write("x1 = ");
+            int x1 = int.Parse(Console.ReadLine());
+            Console.Write("y1 = ");
+            int y1 = int.Parse(Console.ReadLine());
+            Console.Write("x2 = ");
+            int x2 = int.Parse(Console.ReadLine());
+            Console.Write("y2 = ");
+            int y2 = int.Parse(Console.ReadLine());
+
+            if (x >= x1 && x <= x2 && y >= y1 && y <= y2)
+            {
+                Console.WriteLine("inside");
+            }
+            else
+            {
+                Console.WriteLine("outside");
+            }
+        }
+    }
+}
