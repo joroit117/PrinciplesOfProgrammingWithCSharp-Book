6d238a6f92b3f114691e44f35a7feb9b9e351a67
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-03-25 14:31:52 +0200

    Add files via upload

---
+++ Theme 2 - PrimitiveTypesAndVariables/task14/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace task14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isPrime = true;
+            int n = int.Parse(Console.ReadLine());
+            
+            for(int i = 2; i < Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                    isPrime = false;
+            }
+            if(isPrime == true)
+            {
+                Console.WriteLine("Prime");
+            }
+            else
+            {
+                Console.WriteLine("Non-prime");
+            }
+        }
+    }
+}
