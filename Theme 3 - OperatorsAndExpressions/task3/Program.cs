83d3a052f880a898240af86ccdff51dabfd18ef2
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-03-27 19:15:12 +0200

    Add files via upload

---
+++ Theme 3 - OperatorsAndExpressions/task3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            a /= 100;
+            int b = a % 10;
+            if(b == 7)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+        }
+    }
+}
