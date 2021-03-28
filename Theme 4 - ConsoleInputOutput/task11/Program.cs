d166e422f52e10bc3b77f32726c805bcfd55003e
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-03-28 12:26:57 +0300

    Add files via upload

---
+++ Theme 4 - ConsoleInputOutput/task11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace task11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fibonachi
+
+            long a = 0;
+            long b = 1;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            for(int i = 2; i <= 100; i++)
+            {
+                long c = a;
+                a = b;
+                b = b + c;
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
