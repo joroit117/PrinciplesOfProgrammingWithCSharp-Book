c201eae8702af0e9e16301d3c00018712de0576d
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-03-28 12:31:33 +0300

    Add files via upload

---
+++ Theme 4 - ConsoleInputOutput/task2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("S = " + Math.PI * Math.Pow(r,2));
+            Console.WriteLine("P = " + 2 * Math.PI * r);
+        }
+    }
+}
