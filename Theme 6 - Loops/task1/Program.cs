ad20941866ad48c765d1119bdbc37dea6b403b59
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-04-04 12:54:05 +0300

    Add files via upload

---
+++ Theme 6 - Loops/task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
