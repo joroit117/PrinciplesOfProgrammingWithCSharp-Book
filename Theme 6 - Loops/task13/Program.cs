c0f22bd86ade6bc43cf8b218ec2747bb4b25d183
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-04-04 12:45:56 +0300

    Add files via upload

---
+++ Theme 6 - Loops/task13/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace task13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            double n = 0;
+            int e = s.Length-1;
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                int k = 0;
+                if (c == 'A' || c == 'B' || c == 'C' || c == 'D' || c == 'E' || c == 'F')
+                {
+                    switch (c)
+                    {
+                        case 'A':
+                            k = 10;
+                            break;
+                        case 'B':
+                            k = 11;
+                            break;
+                        case 'C':
+                            k = 12;
+                            break;
+                        case 'D':
+                            k = 13;
+                            break;
+                        case 'E':
+                            k = 14;
+                            break;
+                        case 'F':
+                            k = 15;
+                            break;
+                    }
+                }
+                else
+                {
+                    k = int.Parse(c.ToString());
+                }
+                n += k * Math.Pow(16, e);
+                e--;
+            }
+            Console.WriteLine(n);
+        }
+    }
+}
---
+++ Theme 6 - Loops/task14/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace task14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string s = "";
+            string h = "";
+            while (n > 0)
+            {
+                    int k = n % 16;
+                    if(k >= 10 && k <= 15)
+                    {
+                        switch(k)
+                        {
+                        case 10:
+                            s += "A";
+                            break;
+                        case 11:
+                            s += "B";
+                            break;
+                        case 12:
+                            s += "C";
+                            break;
+                        case 13:
+                            s += "D";
+                            break;
+                        case 14:
+                            s += "E";
+                            break;
+                        case 15:
+                            s += "F";
+                            break;
+                        }
+                    }
+                else
+                {
+                    s += (n % 16).ToString();
+                }
+                n /= 16;
+            }
+
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                char c = s[i];
+                h += c;
+            }
+            Console.WriteLine(h);
+        }
+    }
+}
