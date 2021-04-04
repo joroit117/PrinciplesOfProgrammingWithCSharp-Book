b0c96a18d40b5ef3f115553ddb320ce9d8c3509a
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-04-04 12:52:20 +0300

    Add files via upload

---
+++ Theme 6 - Loops/task5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int a = 0;
+            int b = 1;
+            Console.Write(a + " ");
+            Console.Write(b + " ");
+            for(int i = 1; i < n; i++)
+            {
+                Console.Write(a + b + " ");
+                int c = a;
+                a = b;
+                b = c + b;
+            }
+        }
+    }
+}
---
+++ Theme 6 - Loops/task6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            int nFak = 1;
+            int kFak = 1;
+
+            for(int i = n; i > 0; i--)
+            {
+                nFak *= i;
+            }
+            for (int i = k; i > 0; i--)
+            {
+                kFak *= i;
+            }
+
+            Console.WriteLine(nFak / kFak);
+        }
+    }
+}
