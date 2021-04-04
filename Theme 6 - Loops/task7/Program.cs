f5c5dd552c708862ff15f6a4a349c1ea5b3552f6
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-04-04 12:50:42 +0300

    Add files via upload

---
+++ Theme 6 - Loops/task7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            int a = n - k;
+            int nFak = 1;
+            int kFak = 1;
+            int aFak = 1;
+
+            for (int i = n; i > 0; i--)
+            {
+                nFak *= i;
+            }
+            for (int i = k; i > 0; i--)
+            {
+                kFak *= i;
+            }
+            for (int i = a; i > 0; i--)
+            {
+                aFak *= i;
+            }
+
+            Console.WriteLine(nFak * kFak / aFak);
+        }
+    }
+}
---
+++ Theme 6 - Loops/task8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            long nMult2Fak = 1;
+            long nPlus1Fak = 1;
+            long nFak = 1;
+
+            for (int i = n; i > 0; i--)
+            {
+                nFak *= i;
+            }
+            for (int i = n + 1; i > 0; i--)
+            {
+                nPlus1Fak *= i;
+            }
+            for (int i = n * 2; i > 0; i--)
+            {
+                nMult2Fak *= i;
+            }
+
+            Console.WriteLine(nMult2Fak / (nPlus1Fak * nFak));
+        }
+    }
+}
