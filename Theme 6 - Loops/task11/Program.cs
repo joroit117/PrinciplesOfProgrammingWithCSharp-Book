73a73f216c5e11a9b84585d3147850eae0ae1c72
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-04-04 12:47:44 +0300

    Add files via upload

---
+++ Theme 6 - Loops/task11/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace task11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            long nFak = 1;
+
+            for (int i = n; i > 0; i--)
+            {
+                nFak *= i;
+            }
+            int br = 0;
+            string s = nFak.ToString();
+            int length = s.Length;
+            while(true)
+            {
+                char c = s[length-1];
+                if(c == '0')
+                {
+                    br++;
+                }
+                else
+                {
+                    break;
+                }
+                length--;
+            }
+            Console.WriteLine(br);
+        }
+    }
+}
---
+++ Theme 6 - Loops/task12/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace task12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string s = "";
+            string h = "";
+            if (n == 0)
+            {
+                s = "0000";
+            }
+            else if(n == 1)
+            {
+                h = "0001";
+            }
+            else if(n == 2)
+            {
+                h = "0010";
+            }
+            else if(n == 3)
+            {
+                h = "0011";
+            }
+            else if(n == 4)
+            {
+                h = "0100";
+            }
+            else if(n == 5)
+            {
+                h = "0101";
+            }
+            else if (n == 6)
+            {
+                h = "0110";
+            }
+            else if (n == 7)
+            {
+                h = "0111";
+            }
+            else
+            {
+                while (n > 0)
+                {
+                    s += (n % 2).ToString();
+                    n /= 2;
+                }
+                
+                for(int i = s.Length - 1; i >= 0; i--)
+                {
+                    char c = s[i];
+                    h += c;
+                }
+            }
+            Console.WriteLine(h);
+        }
+    }
+}
