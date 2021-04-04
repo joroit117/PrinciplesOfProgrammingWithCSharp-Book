6cf832d99a294d955adaa2427524b1f4e88fb809
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-04-04 12:49:21 +0300

    Add files via upload

---
+++ Theme 6 - Loops/task10 - Matrix/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace task10___Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            int k = n;
+            for(int i = 1; i <= n; i++)
+            {
+                for(int j = i; j <= k; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+                k++;
+            }    
+
+        }
+    }
+}
---
+++ Theme 6 - Loops/task10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace task10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int x = int.Parse(Console.ReadLine());
+            double S = 1;
+            double iFak = 1;
+            double Pow = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                iFak *= i;
+                Console.WriteLine(iFak);
+                Pow *= x;
+                Console.WriteLine(Pow);
+                S += (iFak / Pow); 
+            }
+            Console.WriteLine($"{S:f2}");
+        }
+    }
+}
