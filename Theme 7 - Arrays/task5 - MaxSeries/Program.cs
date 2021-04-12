5954fca082b6e6bf66e27c98376739ddab6b9107
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-04-12 19:57:55 +0300

    Add files via upload

---
+++ Theme 7 - Arrays/task5 - MaxSeries/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace task5___MaxSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+
+            int start = 0;
+            int bestStart = 0;
+            int counter = 1;
+            int maxCounter = Int32.MinValue;
+            bool nonStop = true;
+
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                if (array[i] > array[i + 1])
+                {
+                    if (counter > maxCounter)
+                    {
+                        maxCounter = counter;
+                        bestStart = start;
+                        nonStop = false;
+                    }
+                    start = i + 1;
+                    counter = 0;
+                }
+                counter++;
+            }
+            if (nonStop == true)
+            {
+                maxCounter = n;
+                bestStart = 0;
+            }
+            for (int i = bestStart; i < bestStart + maxCounter; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+    }
+}
