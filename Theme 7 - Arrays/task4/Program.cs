2b8b986e9bc3626f25800450d77f0031a9c55bb2
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-04-12 19:58:52 +0300

    Add files via upload

---
+++ Theme 7 - Arrays/task4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            int[] midarr = new int[n];
+            int[] maxarr = new int[n];
+            int br = 1;
+            int maxbr = Int32.MinValue;
+            for(int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            for(int i = 0; i < n-1; i++)
+            {
+                if(array[i] == array[i+1])
+                {
+                    midarr[br - 1] = array[i];
+                    midarr[br] = array[i + 1];
+                    br++;
+                }
+                else
+                {
+                    if(br > maxbr)
+                    {
+                        maxbr = br;
+                        for(int j = 0; j < midarr.Length; j++)
+                        {
+                            maxarr[j] = midarr[j];
+                            midarr[j] = 0;
+                        }
+                        br = 1;
+                    }
+                    else
+                    {
+                        br = 1;
+                        for (int j = 0; j < midarr.Length; j++)
+                        {
+                            midarr[j] = 0;
+                        }
+                    }
+
+                }
+                
+            }
+            for(int i = 0; i < maxbr; i++)
+            {
+                Console.Write(maxarr[i] + " ");
+            }
+        }
+    }
+}
