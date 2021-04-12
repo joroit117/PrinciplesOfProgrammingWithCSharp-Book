27f9f7f11854f26b54b1ce11236c258df41799fb
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-04-12 20:01:12 +0300

    Add files via upload

---
+++ Theme 7 - Arrays/ArrayInversion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ArrayInversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+            int length = array.Length;
+            int[] reversed = new int[length];
+
+            for(int i = 0; i < length; i++)
+            {
+                reversed[length - i - 1] = array[i];
+            }
+            for(int i = 0; i < length; i++)
+            {
+                Console.Write(reversed[i] + " ");
+            }
+        }
+    }
+}
---
+++ Theme 7 - Arrays/ArraySymmetryCheck/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ArraySymmetryCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+
+            for(int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            bool symmetric = true;
+            for(int i = 0; i < array.Length / 2; i++)
+            {
+                if(array[i] != array[n - i - 1])
+                {
+                    symmetric = false;
+                    break;
+                }
+            }
+            Console.WriteLine("Is symmetric? {0}", symmetric);
+        }
+    }
+}
