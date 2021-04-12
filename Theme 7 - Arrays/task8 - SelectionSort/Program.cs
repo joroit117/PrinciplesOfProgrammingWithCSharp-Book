6c73695ab67fbc92e68f5e3f30d63d4d10b64ec9
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-04-12 20:02:26 +0300

    Add files via upload

---
+++ Theme 7 - Arrays/task8 - SelectionSort/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] sorted = new int[n];
+            int min = Int32.MaxValue;
+
+            int[] array = new int[n];
+            for(int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            int index = 0;
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = i; j < n; j++)
+                {
+                    if(array[j] < min)
+                    {
+                        min = array[j];
+                        index = j;
+                    }
+                }
+                swap(ref array[i], ref array[index]);
+                min = Int32.MaxValue;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(array[i]);
+            }
+        }
+        static void swap(ref int a, ref int b)
+        {
+            int c = a;
+            a = b;
+            b = c;
+        }
+    }
+}
