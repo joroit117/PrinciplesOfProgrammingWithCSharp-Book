2d4ce5c12c64a8082d749451a7300765c7b7c7fe
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-04-12 19:55:21 +0300

    Add files via upload

---
+++ Theme 7 - Arrays/task7/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            int biggestSum = 0;
+            int sum = 0;
+            int start = 0;
+
+            int[] array = new int[n];
+            for(int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            for(int i = 0; i < n - k; i++)
+            {
+                for(int j = i; j < k + i; j++)
+                {
+                    sum += array[j];
+                }
+                if(sum > biggestSum)
+                {
+                    biggestSum = sum;
+                    start = i;
+                }
+                sum = 0;
+            }
+            for(int i = start; i < k + start; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+
+        }
+    }
+}
